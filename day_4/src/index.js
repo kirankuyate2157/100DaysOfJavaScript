dca9403f9cbf5e6cad42e55e010d2f89df1f15a4
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-12-13 22:56:33 +0530

    db connected

--- day_4/src/index.js
+++ day_4/src/index.js
@@ -1,1 +1,17 @@
-console.log("Starting ,...")
+import express from 'express';
+import dotenv from "dotenv";
+import connectDB from './db/mongoDB.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hey i back end api started now testing .... 🍻⏳")
+})
+
+
+app.listen(process.env.PORT, () => {
+    connectDB()
+    console.log("listening on port http://localhost:" + process.env.PORT + "  🏃🏻💨 🚀")
+})
