0ffda558d2c263562b3c42edc8ad9241bfcdd047
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2024-01-28 15:08:33 +0530

    vidoe controller done -test pending

--- day_4/src/controllers/video.controller.js
+++ day_4/src/controllers/video.controller.js
@@ -1,26 +1,45 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
-import { User } from "../models/user.model.js";
+// import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+  const {
+    page = 1,
+    limit = 10,
+    query = `/^video/`,
+    sortBy = "createdAt",
+    sortType = 1,
+    userId = req.user._id,
+  } = req.query;
   //TODO: get all videos based on query, sort, pagination
   try {
+    // find user in db
+    const user = await User.findById({
+      _id: userId,
+    });
+
+    if (!user) {
+      throw new ApiError(404, "user not found");
+    }
+
     let videoQuery = { owner: userId };
 
     // If a query string is provided, add it to the videoQuery
     if (query) {
       // Example: if query is a string property, you can search for it in title and description fields
-      videoQuery.$or = [{ title: { $regex: query, $options: 'i' } }, { description: { $regex: query, $options: 'i' } }];
+      videoQuery.$or = [
+        { title: { $regex: query, $options: "i" } },
+        { description: { $regex: query, $options: "i" } },
+      ];
     }
     const videos = await Video.find(videoQuery)
       .limit(Number(limit))
       .skip((page - 1) * Number(limit))
-      .sort({ [sortBy]: sortType === "desc" ? -1 : 1 })
+      .sort({ [sortBy]: sortType === -1 ? -1 : 1 })
       .exec();
 
     if (videos.length === 0) {
@@ -38,6 +57,13 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
   // TODO: get video, upload to cloudinary, create video
+
+  if (!title || title?.trim() === "") {
+    throw new ApiError(400, "Title content is required  🫠");
+  }
+  if (!description || description?.trim() === "") {
+    throw new ApiError(400, "description content is required  🫠");
+  }
 
   const videoLocalPath = req.files?.videoFile[0]?.path;
   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
@@ -58,13 +84,16 @@ const publishAVideo = asyncHandler(async (req, res) => {
     var thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
 
   console.log(" video uploaded :", video);
+  if (!video || !thumbnail) {
+    throw new ApiError(402, "error while uploading video  🫠");
+  }
 
-  const createdVideo = await User.create({
-    videoFile: video.url,
-    thumbnail: thumbnail.url,
+  const createdVideo = await Video.create({
+    videoFile: video?.url,
+    thumbnail: thumbnail?.url,
     title: title,
     description: description,
-    duration: video.duration,
+    duration: video?.duration,
     owner: req.user._id,
   });
 
@@ -146,8 +175,35 @@ const deleteVideo = asyncHandler(async (req, res) => {
     );
 });
 
+// toggle publish status
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "This video id is not valid");
+  }
+
+  // find video in db
+  const video = await Video.findById({
+    _id: videoId,
+  });
+
+  if (!video) {
+    throw new ApiError(404, "video not found");
+  }
+
+  if (video.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You don't have permission to toggle this video!");
+  }
+  // toggle video status
+  video.isPublished = !video.isPublished;
+
+  await video.save({ validateBeforeSave: false });
+
+  //return response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "video toggle successfully!!"));
 });
 
 export {
